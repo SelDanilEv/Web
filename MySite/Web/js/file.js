2b018429cfca881f429b594392f357048e2d02ea
SelDanilEv <47692381+SelDanilEv@users.noreply.github.com>
2021-07-03 22:08:36 +0300

    Updt

---
+++ MySite/Web/js/file.js
@@ -0,0 +1,12 @@
+'use strict'
+
+// describe("pow", function() {
+//     it("возводит в степень n", function() {
+//         assert.equal(pow(2, 3), 8);
+//     });
+//
+//     it("возводит в степень n", function() {
+//         assert.equal(pow(3, 3), 27);
+//     });
+//
+// });
